11386fe0b8ca37924fcc572a57a1b641ee98899c
shivamkumarlnct <shivamsitu2@gmail.com>
2024-06-30 11:09:47 +0530

    new

--- JAVABasic/Hasmap/interation.java
+++ JAVABasic/Hasmap/interation.java
@@ -1,5 +1,22 @@
 package Hasmap;
+import java.util.*;
 
 public class interation {
+    public static void main(String args[]){
+        HashMap<String,Integer>hm=new HashMap<>();
+        hm.put("india", 100);
+        hm.put("indonesia", 10);
+        hm.put("china", 1000);
+        hm.put("USA", 1010);
+        hm.put("UK", 11100);
+        hm.put("nepal", 1090);
+        hm.put("korea", 16800);
+
+        System.out.println(hm);
+
+        // iteration
+        
+
+    }
     
 }
