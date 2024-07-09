31ab672f8efd371a8a7d98bbb1c68ec3cef021d5
shivamkumarlnct <shivamsitu2@gmail.com>
2024-07-09 23:11:31 +0530

    new

---
+++ JAVABasic/Hasmap/tries/countprefix.java
@@ -0,0 +1,8 @@
+package Hasmap.tries;
+
+public class countprefix {
+    public static void main(String args[]){
+        
+    }
+    
+}
---
+++ JAVABasic/Hasmap/tries/startwith.java
@@ -0,0 +1,64 @@
+package Hasmap.tries;
+
+public class startwith {
+    static class Node {
+        Node children[] = new Node[26];
+        boolean eow = false;
+
+        Node() {
+            for (int i = 0; i < 26; i++) {
+                children[i] = null;
+            }
+        }
+    }
+
+    public static Node root = new Node();
+
+    public static void insert(String word) {
+        Node curr = root;
+        for (int i = 0; i < word.length(); i++) {
+            int idx = word.charAt(i) - 'a';
+            if (curr.children[idx] == null) {
+                curr.children[idx] = new Node();
+            }
+            curr = curr.children[idx];
+
+        }
+        curr.eow = true;
+    }
+
+    public static boolean search(String key) {
+        Node curr = root;
+        for (int level = 0; level < key.length(); level++) {
+            int idx = key.charAt(level) - 'a';
+            if (curr.children[idx] == null) {
+                return false;
+            }
+            curr = curr.children[idx];
+        }
+        return curr.eow;
+    }
+
+    public static boolean startwith(String prefix) {
+        Node curr = root;
+        for (int i = 0; i < prefix.length(); i++) {
+            int idx = prefix.charAt(i) - 'a';
+            if (curr.children[idx] == null) {
+                return false;
+            }
+            curr = curr.children[idx];
+        }
+        return true;
+    }
+
+    public static void main(String args[]) {
+        String word[] = { "apple", "app", "mango", "man", "women" };
+        String pre1 = "apple";
+        String pre2 = "moon";
+        for (int i = 0; i < word.length; i++) {
+            insert(word[i]);
+        }
+        System.out.println(startwith(pre1)); 
+        System.out.println(startwith(pre2)); 
+    }
+}
